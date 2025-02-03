697196452d39d61e3508880d8d6603555ebab046
eloise <eloise.ansoult@student.junia.com>
2025-02-03 11:56:19 +0100

    save

--- TP_prog2.c
+++ TP_prog2.c
@@ -4,59 +4,34 @@
 #include <windows.h>
 
 typedef struct {
-    char* nom;
     int frequence;
     int duree;
 } Note;
 
 Note* creerNote(char* nom, int frequence, int duree) {
     Note* note = (Note*)malloc(sizeof(Note));
-    note->nom = nom;
     note->frequence = frequence;
     note->duree = duree;
     return note;
 }
 
-void jouerSon(int frequence, int duree) {
-    Beep(frequence, duree);
-}
-
 void simulerPiano(Note** notes, int nbNotes) {
-    char touches[] = {'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'm', 'l'};
-    while (1) {
-        if (_kbhit()) {
-            char touche = _getch();
-            if (touche == 'q') {
-                break;
-            }
-            for (int i = 0; i < nbNotes; ++i) {
-                if (touche == touches[i]) {
-                    jouerSon(notes[i]->frequence, notes[i]->duree);
-                    printf("Note jouée: %s\n", notes[i]->nom);
-                }
-            }
-        }
+    for (int i = 0; i < nbNotes; i++) {
+        Beep(notes[i]->frequence, notes[i]->duree);
     }
+    while (!_kbhit())
+    
 }
 
-void libererNotes(Note** notes, int nombreNotes) {
-    for (int i = 0; i < nombreNotes; ++i) {
+void libererNotes(Note** notes, int nbNotes) {
+    for (int i = 0; i < nbNotes; i++) {
         free(notes[i]);
     }
 }
 
 int main() {
     float frequencies[12] = {261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88};
-    char* noms[12] = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
     int duree = 500;
 
-    Note* notes[12];
-    for (int i = 0; i < 12; ++i) {
-        notes[i] = creerNote(noms[i], (int)frequencies[i], duree);
-    }
 
-    simulerPiano(notes, 12);
-    libererNotes(notes, 12);
-
-    return 0;
 }
---
+++ tp_pointeur_dynamique.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+typedef struct {
+    int frequence;
+    int duree;
+} Note;
+
+Note* creerNote(int frequence, int duree) {
+    Note* nouvelleNote = (Note*)malloc(sizeof(Note));
+    nouvelleNote->frequence = frequence;
+    nouvelleNote->duree = duree;
+    return nouvelleNote;
+}
+
+void jouerSon(int frequence, int duree) {
+    Beep(frequence, duree);
+}
+
+void simulerPiano(Note** notes, int nombreNotes) {
+    char touches[] = {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '\\'};
+    while (1) {
+        if (_kbhit()) {
+            char touche = _getch();
+            if (touche == 'q') {
+                break;
+            }
+            for (int i = 0; i < nombreNotes; ++i) {
+                if (touche == touches[i]) {
+                    jouerSon(notes[i]->frequence, notes[i]->duree);
+                    printf("Note jouée: %d Hz\n", notes[i]->frequence);
+                }
+            }
+        }
+    }
+}
+
+void libererNotes(Note** notes, int nombreNotes) {
+    for (int i = 0; i < nombreNotes; ++i) {
+        free(notes[i]);
+    }
+}
+
+int main() {
+    float frequencies[12] = {261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88};
+    int duree = 500;
+
+    Note* notes[12];
+    for (int i = 0; i < 12; ++i) {
+        notes[i] = creerNote((int)frequencies[i], duree);
+    }
+
+    simulerPiano(notes, 12);
+    libererNotes(notes, 12);
+
+    return 0;
+}
