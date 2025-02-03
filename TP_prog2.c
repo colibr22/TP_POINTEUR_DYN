4f20a3fed72e5e0781f0f8ca46ccc639d80434c1
eloise <eloise.ansoult@student.junia.com>
2025-02-03 11:08:48 +0100

    void simuler

--- TP_prog2.c
+++ TP_prog2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <conio.h>
 
 typedef struct {
     int frequence;
@@ -17,8 +19,39 @@ Note* creer_note(int frequence, int duree) {
     note->duree = duree;
     return note;
 }
-
-void liberer_note(Note* note) {
-    free(note);
+void jouerSon(int frequence) {
+    Beep(frequence, 500);
 }
 
+void simulerPiano(Note** notes, int nbNotes) {
+    char touches[] = {'a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'm', 'l', '\'', '\\'};
+    while (1) {
+        if (_kbhit()) {
+            char touche = _getch();
+            if (touche == 'q') {
+                break;
+            }
+            for (int i = 0; i < nbNotes; ++i) {
+                if (touche == touches[i]) {
+                    jouerSon(notes[i]->frequence);
+                    printf("Note jouée: %s\n", notes[i]->nom);
+                }
+            }
+        }
+    }
+}
+
+void libererNotes(Note **notes, int nbNotes){
+    for (int i = 0; i < nbNotes; i++){
+        free(notes[i]);
+    }
+    free(notes);
+}
+
+int main() {
+    float frequence[12] = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 }; 
+    char* noms[12] = { "Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si" };
+
+
+    return 0;
+}
